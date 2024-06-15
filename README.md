799391a7b143491a38f97283ea2c05b4fc9b31cd
Springcomp <springcomp@users.noreply.github.com>
2024-06-15 08:24:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an attempt to create a C# custom action for use in WiX
+that helps register custom keyboard layouts in Windows.
---
+++ src/KbdMsi/CustomActions.cs
@@ -0,0 +1,23 @@
+﻿using WixToolset.Dtf.WindowsInstaller;
+
+namespace KbdMsi
+{
+	public class CustomActions
+	{
+		[CustomAction("SampleCA")]
+		public static ActionResult SampleCustomAction2(Session session)
+		{
+			using (Record msgRec = new Record(0))
+			{
+				msgRec[0] = "Hello from SampleCA!";
+				session.Message(InstallMessage.Info, msgRec);
+				session.Message(InstallMessage.User, msgRec);
+			}
+			session.Log("Testing summary info...");
+			SummaryInfo summInfo = session.Database.SummaryInfo;
+			session.Log("MSI PackageCode = {0}", summInfo.RevisionNumber);
+			session.Log("MSI ModifyDate = {0}", summInfo.LastSaveTime);
+			return ActionResult.UserExit;
+		}
+	}
+}
