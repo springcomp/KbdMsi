5b0f75cde474ceba70a0526422846a3b1a04f45f
Springcomp <springcomp@users.noreply.github.com>
2024-06-15 17:52:42 +0200

    [wix] Set property to custom action

--- src/KbdMsi/CustomActions.cs
+++ src/KbdMsi/CustomActions.cs
@@ -4,19 +4,25 @@ namespace KbdMsi
 {
 	public class CustomActions
 	{
-		[CustomAction("SampleCA")]
-		public static ActionResult SampleCustomAction(Session session)
+		[CustomAction("CA01")]
+		public static ActionResult RegisterKeyboard(Session session)
 		{
-			using (Record msgRec = new Record(0))
-			{
-				msgRec[0] = "Hello from SampleCA!";
-				session.Message(InstallMessage.Info, msgRec);
-				session.Message(InstallMessage.User, msgRec);
-			}
-			session.Log("Testing summary info...");
-			SummaryInfo summInfo = session.Database.SummaryInfo;
-			session.Log("MSI PackageCode = {0}", summInfo.RevisionNumber);
-			session.Log("MSI ModifyDate = {0}", summInfo.LastSaveTime);
+			var customActionData = session["CustomActionData"];
+			var array = customActionData.Split('|');
+			var filePath = array[0];
+			var lcid = array[1];
+
+			session.Log("Registering keyboard layout.");
+			session.Log("DLL = {0}", filePath);
+			session.Log("LCIDValue = {0}", lcid);
+
+			return ActionResult.Success;
+		}
+
+		[CustomAction("CA02")]
+		public static ActionResult UnregisterKeyboard(Session session)
+		{
+			// TODO: how do we know which keyboard layout needs to be unregistered?
 			return ActionResult.Success;
 		}
 	}
